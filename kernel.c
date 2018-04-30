0f46a49db741cf5cb288563f8b05c9351dd33ae2
cth103 <cthdev@email.com>
2018-04-30 16:40:45 +0100

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -43,7 +43,13 @@ uint8_t color;
 /* Note the use of the volatile keyword to prevent the compiler from eliminating dead stores. */
 volatile uint16_t* buffer;
 extern void entering_v86(uint32_t ss, uint32_t esp, uint32_t cs, uint32_t eip);
-
+size_t strlen(const char* str) 
+  {
+  	size_t len = 0;
+  	while (str[len])
+  		len++;
+  	return len;
+  }
 /* get functions */
 uint32_t getesp( void )
 {
