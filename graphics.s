b0eebe7db928059d08323ccffa064ca35aa2d33e
cth103 <cthdev@email.com>
2018-04-30 17:47:07 +0100

    Update graphics.s

--- graphics.s
+++ graphics.s
@@ -1,11 +1,9 @@
 .global int13
 .intel_syntax noprefix
 int13:
-        cli
-        mov     eax,cr0
-        and     al,not 1
-        mov     cr0,eax
-        sti
+        mov eax, cr0
+        or eax, 1
+        mov cr0, eax
         mov ah, 0
         mov al, 0x13
         int 0x10
