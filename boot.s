cf36f624df9935e35fef3b169e174eb9b969db8c
cth103 <cthdev@email.com>
2018-04-30 12:04:46 +0100

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,16 +1,6 @@
 /* Declare constants for the multiboot header. */
 [bits 16]
-.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
-.set MEMINFO,  1<<1             /* provide memory map */
-.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
-.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
-.set CHECKSUM, -(MAGIC + FLAGS) /
 
-.section .multiboot
-.align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
 
 
 .section .bss
@@ -22,43 +12,10 @@ stack_top:
 .section .text
 .global _start
 .type _start, @function
-idt_real:
-	dw 0x3ff		; 256 entries, 4b each = 1K
-	dd 0			; Real Mode IVT @ 0x0000
- 
-savcr0:
-	dd 0			; Storage location for pmode CR0.
-GoRMode:
-	mov sp, 0x8000		; pick a stack pointer.
-	mov ax, 0		; Reset segment registers to 0.
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	lidt [idt_real]
-	sti			; Restore interrupts -- be careful, unhandled int's will kill it.
+
 _start:
  
-	cli			; Disable interrupts.
- 
-	; Need 16-bit Protected Mode GDT entries!
-	mov eax, DATASEL16	; 16-bit Protected Mode data selector.
-	mov ds, eax
-	mov es, eax
-	mov fs, eax
-	mov gs, eax
-	mov ss, eax
- 
- 
-	; Disable paging (we need everything to be 1:1 mapped).
-	mov eax, cr0
-	mov [savcr0], eax	; save pmode CR0
-	and eax, 0x7FFFFFFe	; Disable paging bit & disable 16-bit pmode.
-	mov cr0, eax
- 
-	jmp 0:GoRMode		; Perform Far jump to set CS.
-
+	
 		/*
 	     AL  Type     Format   Cell  Colors        Adapter  Addr  Monitor
       0  text     40x25     8x8* 16/8 (shades) CGA,EGA  b800  Composite
