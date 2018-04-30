d07e25c945698adf6ea4926de82db1ecf87dd5c4
cth103 <cthdev@email.com>
2018-04-30 17:43:25 +0100

    Update challenge1.bat

--- challenge1.bat
+++ challenge1.bat
@@ -1,5 +1,5 @@
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-as graphics.s -o graphics.o
 i686-elf-as boot.s -o boot.o
-i686-elf-gcc -T linker.ld -o ihavenoidea.bin -ffreestanding -O2 -nostdlib boot.o graphics.o V86.o -lgcc
+i686-elf-gcc -T linker.ld -o ihavenoidea.bin -ffreestanding -O2 -nostdlib boot.o graphics.o kernel.o -lgcc
 qemu-system-i386 -kernel ihavenoidea.bin
