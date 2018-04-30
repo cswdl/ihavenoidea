905e19027fc77a9fef8fb11dc2ca2c14d993fcf3
cth103 <cthdev@email.com>
2018-04-30 11:51:42 +0100

    Update boot.s

--- boot.s
+++ boot.s
@@ -23,6 +23,32 @@ stack_top:
 .type _start, @function
 _start:
 	mov $stack_top, %esp
+		/*
+	     AL  Type     Format   Cell  Colors        Adapter  Addr  Monitor
+      0  text     40x25     8x8* 16/8 (shades) CGA,EGA  b800  Composite
+      1  text     40x25     8x8* 16/8          CGA,EGA  b800  Comp,RGB,Enh
+      2  text     80x25     8x8* 16/8 (shades) CGA,EGA  b800  Composite
+      3  text     80x25     8x8* 16/8          CGA,EGA  b800  Comp,RGB,Enh
+      4  graphic  320x200   8x8  4             CGA,EGA  b800  Comp,RGB,Enh
+      5  graphic  320x200   8x8  4 (shades)    CGA,EGA  b800  Composite
+      6  graphic  640x200   8x8  2             CGA,EGA  b800  Comp,RGB,Enh
+      7  text     80x25    9x14* 3 (b/w/bold)  MDA,EGA  b000  TTL Mono
+      8,9,0aH  PCjr modes
+      0bH,0cH  (reserved; internal to EGA BIOS)
+      0dH graphic  320x200   8x8  16            EGA,VGA  a000  Enh,Anlg
+      0eH graphic  640x200   8x8  16            EGA,VGA  a000  Enh,Anlg
+      0fH graphic  640x350  8x14  3 (b/w/bold)  EGA,VGA  a000  Enh,Anlg,Mono
+      10H graphic  640x350  8x14  4 or 16       EGA,VGA  a000  Enh,Anlg
+      11H graphic  640x480  8x16  2             VGA      a000  Anlg
+      12H graphic  640x480  8x16  16            VGA      a000  Anlg
+      13H graphic  640x480  8x16  256           VGA      a000  Anlg
+      check:http://webpages.charter.net/danrollins/techhelp/0113.HTM
+	*/
+	
+	mov $00, %ah //Int 10/AH=00h = SET VIDEO MODE, setting ah to 00
+	mov $0x13, %al //Int 10/AL=13 = DESIRED VIDEO MDOE, setting to 13h for 2 5 6 colors
+	int $0x10 //finally, call int 10
+	ret // return from procedure
 	call main
 	cli
 1:	hlt
