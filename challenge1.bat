c0da4584461c21e836630a459c3af84c0885353e
cth103 <cthdev@email.com>
2018-04-30 14:49:14 +0100

    Update challenge1.bat

--- challenge1.bat
+++ challenge1.bat
@@ -1,6 +1,6 @@
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -c video.c -o video.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-as V86.asm -o V86.o
+i686-elf-as V86.asm -o V86.o -msyntax=intel
 i686-elf-as boot.s -o boot.o
 i686-elf-gcc -T linker.ld -o ihavenoidea.bin -ffreestanding -O2 -nostdlib boot.o kernel.o video.o V86.o -lgcc
 qemu-system-i386 -kernel ihavenoidea.bin
