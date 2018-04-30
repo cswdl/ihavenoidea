551b9434c6421b8e5fdb049917420b345a304b69
cth103 <cthdev@email.com>
2018-04-30 14:46:36 +0100

    Update V86.asm

--- V86.asm
+++ V86.asm
@@ -1,14 +1,13 @@
-; you should declare this function as :
-; extern void entering_v86(uint32_t ss, uint32_t esp, uint32_t cs, uint32_t eip);
+.intel_syntax noprefix
+
 global entering_v86
 
 entering_v86:
-   mov ebp, esp               ; save stack pointer
-
-   push dword  [ebp+4]        ; ss
-   push dword  [ebp+8]        ; esp
-   pushfd                     ; eflags
-   or dword [esp], (1 << 17)  ; set VM flags
-   push dword [ebp+12]        ; cs
-   push dword  [ebp+16]       ; eip
+   mov ebp, esp               
+   push dword  [ebp+4]        
+   push dword  [ebp+8]        
+   pushfd 
+   or dword [esp], (1 << 17) 
+   push dword [ebp+12] 
+   push dword  [ebp+16]
    iret
