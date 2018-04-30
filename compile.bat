c40ffea244ab53d4bea82eda9fc366607b5d7e6f
cth103 <cthdev@email.com>
2018-04-30 12:23:07 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c video.c -o video.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-as boot.s -o boot.o
-i686-elf-gcc -T linker.ld -o ihavenoidea.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686-elf-gcc -T linker.ld -o ihavenoidea.bin -ffreestanding -O2 -nostdlib boot.o kernel.o video.o -lgcc
 qemu-system-i386 -kernel ihavenoidea.bin
