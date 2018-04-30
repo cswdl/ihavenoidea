17771bc2a229e64aeebbb84f205181d16eb2752b
cth103 <cthdev@email.com>
2018-04-30 18:07:42 +0100

    Update unrealMode.s

--- unrealMode.s
+++ unrealMode.s
@@ -1,34 +1,14 @@
 .global startUnrealMode
 .intel_syntax noprefix
 startUnrealMode:   
-   xor ax, ax       ; make it zero
-   mov ds, ax             ; DS=0
-   mov ss, ax             ; stack starts at seg 0
-   mov sp, 0x9c00         ; 2000h past code start, 
-                          ; making the stack 7.5k in size
- 
-   cli                    ; no interrupts
-   push ds                ; save real mode
- 
-   lgdt [gdtinfo]         ; load gdt register
- 
-   mov  eax, cr0          ; switch to pmode by
-   or al,1                ; set pmode bit
+    
+startUnrealMode:   
+   xor ax, ax 
    mov  cr0, eax
- 
-   jmp $+2                ; tell 386/486 to not crash
- 
-   mov  bx, 0x08          ; select descriptor 1
-   mov  ds, bx            ; 8h = 1000b
+   jmp 0x8:pmode
+pmode:
+   mov  bx, 0x10          ; select descriptor 2, instead of 1
+   mov  ds, bx            ; 10h = 10000b
  
    and al,0xFE            ; back to realmode
    mov  cr0, eax          ; by toggling bit again
- 
-   pop ds                 ; get back old segment
-   sti
- 
-   mov bx, 0x0f01         ; attrib/char of smiley
-   mov eax, 0x0b8000      ; note 32 bit offset
-   mov word [ds:eax], bx
- 
-   jmp $                  ; loop forever
