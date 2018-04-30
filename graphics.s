14ff25d5193987eaa8b79d1b981d275e9ed066b9
cth103 <cthdev@email.com>
2018-04-30 17:33:03 +0100

    Update graphics.s

--- graphics.s
+++ graphics.s
@@ -4,9 +4,9 @@ int13:
         movl    %cr0,%eax
         orl     $1,%eax
         movl    %eax,%cr0
-        jmp     cs:@setgraphics
-@setgraphics:
+        jmp     cs:setgraphics
+setgraphics:
         movb    $0,%ah
         movb    $0x13,%al
         int     $0x10
-        re
+        ret
