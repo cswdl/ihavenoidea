c83b707e81fd8cad633eab7854b5b9c37e096f44
cth103 <cthdev@email.com>
2018-04-30 15:06:06 +0100

    Update V86.s

--- V86.s
+++ V86.s
@@ -4,7 +4,9 @@ entering_v86:
         movl    %esp,%ebp
         pushl   4(%ebp)
         pushl   8(%ebp)
+        .intel_syntax noprefix
         pushfd
+        .att_syntax noprefix
         orl     $(1 << 17),(%esp)
         pushl   12(%ebp)
         pushl   16(%ebp)
