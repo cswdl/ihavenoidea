aba6e379f2b8e02681643007832e49f98c1fcd09
cth103 <cthdev@email.com>
2018-04-30 18:22:56 +0100

    Update unrealMode.s

--- unrealMode.s
+++ unrealMode.s
@@ -7,8 +7,7 @@ startUnrealMode:
    mov  cr0, eax
    jmp 0x8:pmode
 pmode:
-   mov  bx, 0x10          ; select descriptor 2, instead of 1
-   mov  ds, bx            ; 10h = 10000b
- 
-   and al,0xFE            ; back to realmode
-   mov  cr0, eax          ; by toggling bit again
+   mov  bx, 0x10 
+   mov  ds, bx
+   and al,0xFE
+   mov  cr0, eax
