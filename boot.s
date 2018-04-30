491bfe5b6e560578218cb7df6c278dadafe7bfb6
cth103 <cthdev@email.com>
2018-04-30 12:05:50 +0100

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,3 +1,14 @@
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
 
 .section .bss
 .align 16
